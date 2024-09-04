9a49448f8447fab1a8e206ef02383349411456ac
Roger <rkstager@gmail.com>
2024-09-04 10:10:02 -0700

    added tinystories data prepare and config files

---
+++ config/train_tinystories_char_m.py
@@ -0,0 +1,37 @@
+# train a miniature character-level tiny-stories model
+# good for debugging and playing on macbooks and such
+
+out_dir = 'out-tinystories-char_m'
+eval_interval = 1000 # keep frequent because we'll overfit
+eval_iters = 1000
+log_interval = 10 # don't print too too often
+
+# we expect to overfit on this small dataset, so only save when val improves
+always_save_checkpoint = True
+
+wandb_log = True # override via command line if you like
+wandb_project = 'tinystories-char_m'
+wandb_run_name = 'mini-gpt'
+
+dataset = 'tinystories_char'
+gradient_accumulation_steps = 10
+batch_size = 64
+block_size = 512 # context of up to 256 previous characters
+
+# baby GPT model :)
+n_layer = 8
+n_head = 8
+n_embd = 576
+dropout = 0.2
+
+learning_rate = 2e-3 # with baby networks can afford to go a bit higher
+max_iters = 100000
+lr_decay_iters =5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100 # not super necessary potentially
+
+# on macbook also add
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
